900c56245f13b911edfd4b2a4ff5405b9969f7e2
Deepesh297 <deepesh.katudia15769@sakec.ac.in>
2024-10-11 15:13:15 -0400

    Updated logo and link

--- src/App.js
+++ src/App.js
@@ -7,15 +7,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Failed to download the default logo that has been provided
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://www.propelleraero.com/dirtmate/"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn More
         </a>
       </header>
     </div>
